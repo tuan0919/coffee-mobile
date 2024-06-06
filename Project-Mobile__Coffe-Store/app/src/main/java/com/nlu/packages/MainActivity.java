e8aad701a62cc8e69efca22798b7d8e27ff1c63f
21130530_phamnhuttan <21130530@st.hcmuaf.edu.vn>
2024-06-06 16:17:34 +0700

    refactor: tạo thêm fragment order, cart, home, reward, store #2

--- Project-Mobile__Coffe-Store/app/src/main/java/com/nlu/packages/MainActivity.java
+++ Project-Mobile__Coffe-Store/app/src/main/java/com/nlu/packages/MainActivity.java
@@ -19,16 +19,15 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_reward, R.id.navigation_order,
+                R.id.navigation_cart, R.id.navigation_store)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
 }
