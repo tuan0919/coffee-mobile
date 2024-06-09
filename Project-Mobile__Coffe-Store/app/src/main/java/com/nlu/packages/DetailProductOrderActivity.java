93c6f32e363ad9d866f71b9397d3d32f13cd2b70
PhamPhiTruong <21130593@st.hcmuaf.edu.vn>
2024-06-09 16:06:10 +0700

    New hard code front end #4

---
+++ Project-Mobile__Coffe-Store/app/src/main/java/com/nlu/packages/DetailProductOrderActivity.java
@@ -0,0 +1,33 @@
+package com.nlu.packages;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DetailProductOrderActivity extends AppCompatActivity {
+    ImageButton btnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_detail_product_order);
+        btnBack = findViewById(R.id.goBackButton);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
